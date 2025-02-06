bd6a3e1d1b9127bbcc00b8791db2de1e4a2c95b3
ftyszyx <whyzi@qq.com>
2025-02-06 21:45:00 +0800

    add xlabeling

--- demo/train.py
+++ demo/train.py
@@ -9,6 +9,16 @@ yolo11n  res:runs\detect\crack_detection14\weights\best.pt
 Class     Images  Instances      Box(P          R      mAP50  mAP50-95): 100%|██████████| 85/85 [00:09<00:00,   
     all       1696       2637      0.579      0.523       0.54      0.241
 
+
+second train:2025-2-6
+yolo11s  res:runs\detect\crack_detection4\weights\best.pt
+YOLO11s summary (fused): 238 layers, 9,414,348 parameters, 0 gradients, 21.3 GFLOPs
+Class     Images  Instances      Box(P          R      mAP50  mAP50-95): 100%|██████████| 66/66 [00:07<00:00,  8.72it/s]
+all       2097       3368      0.559       0.55      0.554      0.258
+纵向裂缝        570        822       0.51      0.499      0.471      0.211
+横向裂缝        449        784       0.48      0.395      0.401      0.157
+龟裂        938       1237      0.621      0.657      0.684      0.358
+坑洞        303        525      0.624      0.649       0.66      0.306
 """
 
 
@@ -20,7 +30,7 @@ def train_model():
         print(f"GPU: {torch.cuda.get_device_name(0)}")
 
     # 使用更大的模型
-    model = YOLO("yolo11s.pt")  # 从n改为m，使用更大的模型
+    model = YOLO("yolo11m.pt")  # 从n改为m，使用更大的模型
 
     # 开始训练
     results = model.train(
